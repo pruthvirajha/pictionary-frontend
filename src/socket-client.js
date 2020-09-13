12801c9ec2d8c42baa076f7fa09f4929d5a4f12b
Vignesh, Narayanasamy <vigneshnv7@gmail.com>
2020-09-13 10:33:36 +0200

    Reads SOCKET_URL from environment variable passed from CLI.

--- src/socket-client.js
+++ src/socket-client.js
@@ -2,10 +2,10 @@ import io from 'socket.io-client';
 import { reconnectionConfig } from './socketConfig'
 
 function connectToSocket() {
-    console.log("Connecting to SOCKET_URL @: " + process.env.SOCKET_URL);
-    const socket = io(process.env.SOCKET_URL, reconnectionConfig);
-    socket.on('connect', (socketResponse) => {
-        console.log("Socket connected" + JSON.stringify(socketResponse));
+    console.log(`Connecting to SOCKET_URL --> ${process.env.REACT_APP_SOCKET_URL}`);
+    const socket = io(process.env.REACT_APP_SOCKET_URL, reconnectionConfig);
+    socket.on('connect', () => {
+        console.log(`Socket connected - OK`);
     });
 }
 
